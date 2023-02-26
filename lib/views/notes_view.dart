2f8dd0b4dc82a07d1d47133a9529b49ec1a2e86e
hamada zakaria <hamada.zakaria00@gmail.com>
2023-02-26 18:19:58 +0200

    add custom notes appbar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
 // import 'package:flutter/src/widgets/framework.dart';
 // import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -7,6 +8,8 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: NoteViewBody(),
+    );
   }
 }
